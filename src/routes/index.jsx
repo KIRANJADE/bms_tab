0095bbc26a62c025466fcc758bcb18ae8402c2aa
karthika <55327368+karthika1607@users.noreply.github.com>
2025-01-15 17:54:31 +0530

    card and table

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,6 +4,8 @@ import LoginLayout from "../layout/Login";
 import DashboardLayout from "../layout/Dashboard";
 import Login from "../pages/login/login";
 import Dashboard from "../pages/dashboard/dashboard";
+import Chantha from "../pages/chantha/chantha";
+
 
 const AppRoutes = () => {
   return (
@@ -14,6 +16,8 @@ const AppRoutes = () => {
       </Route>
       <Route element={<DashboardLayout />}>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/chantha" element={<Chantha />} />
+        
       </Route>
     </Routes>
   );
